4c029c7ef6ccf0e9f79a20a686e95a13042d3a32
jookwonyoung <jooky0327@naver.com>
2022-04-03 17:49:11 +0900

    Api 테스트

--- src/main/java/petmily/controller/PostsApiController.java
+++ src/main/java/petmily/controller/PostsApiController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.*;
 import petmily.controller.dto.PostsResponseDto;
 import petmily.controller.dto.PostsSaveRequestDto;
 import petmily.controller.dto.PostsUpdateRequestDto;
+import petmily.domain.posts.Posts;
 import petmily.service.posts.PostsService;
 
 @RequestMapping("/posts")
@@ -29,4 +30,11 @@ public class PostsApiController {
     public PostsResponseDto findById (@PathVariable Long id){
         return postsService.findById(id);
     }
+
+    //테스트
+    @GetMapping("/test")
+    public Posts testPosts(){
+        return new Posts("테스트제목", "테스트내용", "테스트작성자");
+    }
+
 }
