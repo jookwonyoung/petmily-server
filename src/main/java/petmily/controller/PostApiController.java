3404b5a6725670756cebd0dccc8e5d2f2e3f436c
jookwonyoung <jooky0327@naver.com>
2022-05-06 11:45:22 +0900

    2022/5/6_sec

--- src/main/java/petmily/controller/PostApiController.java
+++ src/main/java/petmily/controller/PostApiController.java
@@ -2,13 +2,16 @@ package petmily.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 import petmily.config.auth.LoginUser;
 import petmily.config.auth.dto.SessionUser;
 import petmily.controller.dto.PostListResponseDto;
 import petmily.controller.dto.PostSaveRequestDto;
 import petmily.service.post.PostService;
 
+import java.io.File;
 import java.util.List;
+import java.util.Random;
 
 @RequestMapping("/api/post")
 @RequiredArgsConstructor
@@ -17,9 +20,33 @@ public class PostApiController {
 
     private final PostService postService;
 
-    @PostMapping("/save")
-    public Long save(@RequestBody PostSaveRequestDto requestDto, @LoginUser SessionUser user){
-        requestDto.setEmail(user.getEmail());
+    @PostMapping("/save")                              //이미지 파일
+    public Long save(@RequestHeader(value="email") String email, @RequestParam("file") MultipartFile files, @RequestParam("postContent") String content){
+        String rootPath = "/home/ec2-user/petmilyServer/step1/imgDB";
+        //String rootPath = "/Users/jookwonyoung/testImg";
+        String emailPath = rootPath + "/" + email;
+        if (!new File(emailPath).exists()) {
+            try{
+                new File(emailPath).mkdir();
+            }
+            catch(Exception e){
+                e.getStackTrace();
+            }
+        }
+        Random random = new Random();
+        String filename = String.valueOf(System.currentTimeMillis())+random.nextInt();
+        String filePath = emailPath + "/" + filename;
+        try {
+            files.transferTo(new File(filePath));
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        PostSaveRequestDto requestDto = new PostSaveRequestDto();
+
+        requestDto.setPostImg(filename);
+        requestDto.setEmail(email);
+        requestDto.setPostContent(content);
         return postService.save(requestDto);
     }
 
