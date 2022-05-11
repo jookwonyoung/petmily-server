8c3b972f157b45d2dceaf9488e44f7df08164f83
wolfdate25 <wolfdate25@naver.com>
2022-05-11 22:40:21 +0900

    특정 ID의 이미지 가져오기

--- src/main/java/petmily/controller/PostApiController.java
+++ src/main/java/petmily/controller/PostApiController.java
@@ -1,18 +1,22 @@
 package petmily.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.util.SerializationUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
-import petmily.controller.dto.*;
+import petmily.controller.dto.PostListResponseDto;
+import petmily.controller.dto.PostResponseDto;
+import petmily.controller.dto.PostSaveRequestDto;
+import petmily.controller.dto.UserSaveRequestDto;
 import petmily.service.post.PostService;
 import petmily.service.user.UserService;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
 
 @RequestMapping("/api/post")
@@ -23,20 +27,20 @@ public class PostApiController {
     private final UserService userService;
     private final PostService postService;
 
-    private String localPath = "/Users/jookwonyoung/Documents/petmily/testImg";
+    private String localPath = "E:\\repo\\capstone\\petmily-spring-server\\imgDB";
     private String ec2Path = "/home/ec2-user/petmilyServer/step1/imgDB";
     String userRootPath;
     String postRootPath;
 
     @PostMapping("/save")
-    public Long save(@RequestHeader(value="email") String email, @RequestParam("userImg") MultipartFile userImg, @RequestParam("postImg") MultipartFile files, @RequestParam("postContent") String content){
+    public Long save(@RequestHeader(value = "email") String email, @RequestParam("userImg") MultipartFile userImg, @RequestParam("postImg") MultipartFile files, @RequestParam("postContent") String content) {
 
-        if(new File(ec2Path+"/user").exists()){
-            userRootPath = ec2Path+"/user";        //ec2-server
-            postRootPath = ec2Path+"/post";        //ec2-server
-        }else{
-            userRootPath = localPath+"/user";     //localhost
-            postRootPath = localPath+"/post";     //localhost
+        if (new File(ec2Path + "/user").exists()) {
+            userRootPath = ec2Path + "/user";        //ec2-server
+            postRootPath = ec2Path + "/post";        //ec2-server
+        } else {
+            userRootPath = localPath + "/user";     //localhost
+            postRootPath = localPath + "/post";     //localhost
         }
 
         UserSaveRequestDto saveRequestDto = new UserSaveRequestDto();
@@ -47,11 +51,11 @@ public class PostApiController {
         PostSaveRequestDto requestDto = new PostSaveRequestDto();
         requestDto.setEmail(email);
         requestDto.setPostContent(content);
-        Long postId =  postService.save(requestDto);    //저장할 postImg(filename)
+        Long postId = postService.save(requestDto);    //저장할 postImg(filename)
 
         String userConType = userImg.getContentType();
         String conType = files.getContentType();
-        if(!(userConType.equals("image/png") || userConType.equals("image/jpeg") || conType.equals("image/png") || conType.equals("image/jpeg"))){
+        if (!(userConType.equals("image/png") || userConType.equals("image/jpeg") || conType.equals("image/png") || conType.equals("image/jpeg"))) {
             Long error = null;
             return error;
         }
@@ -61,7 +65,7 @@ public class PostApiController {
         try {
             files.transferTo(new File(filePath));
             userImg.transferTo(new File(userFilePath));
-        }catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -69,23 +73,23 @@ public class PostApiController {
     }
 
     @PostMapping("/testSave")
-    public Long save(@RequestHeader(value="email") String email, @RequestParam("postImg") MultipartFile files, @RequestParam("postContent") String content){
+    public Long save(@RequestHeader(value = "email") String email, @RequestParam("postImg") MultipartFile files, @RequestParam("postContent") String content) {
 
-        if(new File(ec2Path+"/user").exists()){
-            userRootPath = ec2Path+"/user";        //ec2-server
-            postRootPath = ec2Path+"/post";        //ec2-server
-        }else{
-            userRootPath = localPath+"/user";     //localhost
-            postRootPath = localPath+"/post";     //localhost
+        if (new File(ec2Path + "/user").exists()) {
+            userRootPath = ec2Path + "/user";        //ec2-server
+            postRootPath = ec2Path + "/post";        //ec2-server
+        } else {
+            userRootPath = localPath + "/user";     //localhost
+            postRootPath = localPath + "/post";     //localhost
         }
 
         PostSaveRequestDto requestDto = new PostSaveRequestDto();
         requestDto.setEmail(email);
         requestDto.setPostContent(content);
-        Long postId =  postService.save(requestDto);    //저장할 postImg(filename)
+        Long postId = postService.save(requestDto);    //저장할 postImg(filename)
 
         String conType = files.getContentType();
-        if(!(conType.equals("image/png") || conType.equals("image/jpeg"))){
+        if (!(conType.equals("image/png") || conType.equals("image/jpeg"))) {
             Long error = null;
             return error;
         }
@@ -93,7 +97,7 @@ public class PostApiController {
         String filePath = postRootPath + "/" + postId;
         try {
             files.transferTo(new File(filePath));
-        }catch(Exception e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
 
@@ -102,10 +106,10 @@ public class PostApiController {
 
 
     @GetMapping(value = "/findById/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
-    public PostResponseDto findById (@PathVariable Long id) throws IOException {
+    public PostResponseDto findById(@PathVariable Long id) throws IOException {
         PostResponseDto tmpDto = postService.findById(id);
 
-        InputStream in = new FileInputStream(localPath+"/post/"+id);   //파일 읽어오기
+        InputStream in = new FileInputStream(localPath + "/post/" + id);   //파일 읽어오기
         byte[] imgByteArray = in.readAllBytes();                       //byte로 변환
         in.close();
 
@@ -121,21 +125,25 @@ public class PostApiController {
         return tmpDto;
     }
 
-    @GetMapping(value = "/getImg", produces = MediaType.IMAGE_JPEG_VALUE)
-    public ResponseEntity<byte[]> getImage() throws IOException {
+    @GetMapping(value = "/getImg/{id}", produces = MediaType.IMAGE_JPEG_VALUE)
+    public ResponseEntity<byte[]> getImage(@PathVariable Long id) {
         InputStream in;
-        try{
-            in = new FileInputStream(ec2Path+"/post/test");   //파일 읽어오기
-        }catch (Exception e){
-            in = new FileInputStream(localPath+"/post/1");   //파일 읽어오기
+        try {
+            try {
+                in = new FileInputStream(ec2Path + "/post/test/" + id);   //파일 읽어오기
+            } catch (Exception e) {
+                in = new FileInputStream(localPath + "/post/1");   //파일 읽어오기
+            }
+            byte[] imgByteArray = in.readAllBytes();                    //byte로 변환
+            in.close();
+            return new ResponseEntity<byte[]>(imgByteArray, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<byte[]>(HttpStatus.INTERNAL_SERVER_ERROR);
         }
-        byte[] imgByteArray = in.readAllBytes();                    //byte로 변환
-        in.close();
-        return new ResponseEntity<byte[]>(imgByteArray, HttpStatus.OK);
     }
 
     @GetMapping(value = "/findAll")
-    public List<PostListResponseDto> findAll(){
+    public List<PostListResponseDto> findAll() {
         List<PostListResponseDto> responseDtoList = postService.findAllDesc();
         return responseDtoList;
     }
