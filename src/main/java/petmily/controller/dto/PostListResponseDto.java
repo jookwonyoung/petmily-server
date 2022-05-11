912d4c76bbe79bcb0603daad4927537c4933cb9a
wolfdate25 <wolfdate25@naver.com>
2022-05-11 22:34:35 +0900

    PostList 응답 객체 복원

--- src/main/java/petmily/controller/dto/PostListResponseDto.java
+++ src/main/java/petmily/controller/dto/PostListResponseDto.java
@@ -3,36 +3,18 @@ package petmily.controller.dto;
 import lombok.Getter;
 import petmily.domain.posts.Post;
 
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
 @Getter
 public class PostListResponseDto {
 
     private Long postId;
     private String email;
-    private byte[] postImg;
+    private String postImg;
     private String postContent;
 
-    public PostListResponseDto(Post entity) {
-
-        String ec2Path = "/home/ec2-user/petmilyServer/step1/imgDB";
-
+    public PostListResponseDto(Post entity){
         this.postId = entity.getPostId();
         this.email = entity.getEmail();
-        try {
-            InputStream in;
-            String img = entity.getPostImg();
-
-            in = new FileInputStream(ec2Path + "/post/test");   //파일 읽어오기
-            this.postImg = in.readAllBytes();
-            in.close();
-
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        this.postImg = entity.getPostImg();
         this.postContent = entity.getPostContent();
     }
 
