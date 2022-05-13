8731e0409a94038df38b659d4dfd025dc5f146c8
jookwonyoung <jooky0327@naver.com>
2022-05-13 10:06:07 +0900

    PostListResponseDto byte 수정

--- src/main/java/petmily/controller/dto/PostListResponseDto.java
+++ src/main/java/petmily/controller/dto/PostListResponseDto.java
@@ -3,18 +3,36 @@ package petmily.controller.dto;
 import lombok.Getter;
 import petmily.domain.posts.Post;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+
 @Getter
 public class PostListResponseDto {
 
     private Long postId;
     private String email;
-    private String postImg;
+    private byte[] postImg;
     private String postContent;
 
-    public PostListResponseDto(Post entity){
+    public PostListResponseDto(Post entity) {
+
+        String ec2Path = "/home/ec2-user/petmilyServer/step1/imgDB";
+
         this.postId = entity.getPostId();
         this.email = entity.getEmail();
-        this.postImg = entity.getPostImg();
+        try {
+            InputStream in;
+            String img = entity.getPostImg();
+
+            in = new FileInputStream(ec2Path + "/post/"+entity.getPostId());   //파일 읽어오기
+            this.postImg = in.readAllBytes();
+            in.close();
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         this.postContent = entity.getPostContent();
     }
 
