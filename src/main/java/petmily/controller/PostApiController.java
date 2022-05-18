e049929b914803c0c282717ec0d279d785457710
jookwonyoung <jooky0327@naver.com>
2022-05-19 01:20:32 +0900

    test2

--- src/main/java/petmily/controller/PostApiController.java
+++ src/main/java/petmily/controller/PostApiController.java
@@ -96,18 +96,15 @@ public class PostApiController {
                 returnMessage = postId.toString();
 
 
-                try{
-                    // 5. 자동 태깅 실행
-                    Thread thread = new Thread(new Runnable() {
-                        @Override
-                        public void run() {
-                            analysisService.autoTagging(postId, detected, filePath);
-                        }
-                    });
-                    thread.start();
-                }catch (Exception e){
-                    returnMessage = "태깅 실패";
-                }
+                // 5. 자동 태깅 실행
+                Thread thread = new Thread(new Runnable() {
+                    @Override
+                    public void run() {
+                        analysisService.autoTagging(postId, detected, filePath);
+                    }
+                });
+                thread.start();
+
 
             } else {
                 returnMessage = "개나 고양이가 없는 사진입니다";
